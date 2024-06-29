b7d76daed4056191558ca75618708ddb43a202d7
Jorge Bravo <jbravoveintemilla@gmail.com>
2024-06-29 16:46:22 -0500

    PANEL DE ADMINISTRACIÓN

--- proyecto001/miapp/admin.py
+++ proyecto001/miapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Articulo, Categoria, Autor
+
 
 # Register your models here.
+class ArticuloAdmin(admin.ModelAdmin):
+    readonly_fields = ('creado','actualizado')
+admin.site.register(Articulo, ArticuloAdmin)
+admin.site.register(Categoria)
+admin.site.register(Autor)
+
+
--- proyecto001/proyecto001/urls.py
+++ proyecto001/proyecto001/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from miapp import views
 
 urlpatterns = [
@@ -43,3 +44,7 @@ urlpatterns = [
     path('create-full-articulo/',views.create_full_articulo, name='create_full_articulo'),
 ]
 
+# Configuración para cargar imágenes
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
